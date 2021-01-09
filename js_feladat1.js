00087ea5bc2255e4f9b2a35cd6cf74749fa7ae45
”Tóth <sandor.toth@hotmail.com>
2021-01-09 13:27:48 +0100

    feladat1

---
+++ js_feladat1.js
@@ -0,0 +1,6 @@
+let name = window.prompt("Hogy hívnak?");
+let age = window.prompt("Mi a kedvenc számod?");
+console.log("Név: "+name+", kedvenc száma: "+age);
+alert("Név: "+name+", kedvenc száma: "+age);
+document.write("Név: "+name+", kedvenc száma: "+age);
+// document.getElementById("feladat1").innerHTML = "Név: " + name + ", kedvenc száma: " + age;
