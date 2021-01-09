b9ba6db47c58977bf56b1f48dd0ab51d77c2f7a5
SanyiToth <52709221+SanyiToth@users.noreply.github.com>
2021-01-09 13:29:10 +0100

    Update js_feladat1.js

--- js_feladat1.js
+++ js_feladat1.js
@@ -3,4 +3,4 @@ let age = window.prompt("Mi a kedvenc számod?");
 console.log("Név: "+name+", kedvenc száma: "+age);
 alert("Név: "+name+", kedvenc száma: "+age);
 document.write("Név: "+name+", kedvenc száma: "+age);
-// document.getElementById("feladat1").innerHTML = "Név: " + name + ", kedvenc száma: " + age;
+// document.getElementById("feladat1").innerHTML = "Név: " + name + ", kedvenc száma: " + age; ????
