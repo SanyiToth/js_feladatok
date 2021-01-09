ae553848312b74f0a8cea31af436b5fa3768a33b
”Tóth <sandor.toth@hotmail.com>
2021-01-09 14:21:28 +0100

    feladat2

---
+++ js_feladat2.js
@@ -0,0 +1,17 @@
+let a = window.prompt("Add meg 'a' értékét!");
+let b = window.prompt("Add meg 'b' értékét!");
+let sum = a + b;
+let mult = a * b;
+let pi = 3.14;
+let c = Math.pow(a, a) + Math.pow(b, b);
+
+console.log(sum);
+console.log(mult);
+console.log(Math.pow(a, a));
+console.log(Math.pow(a, b));
+console.log(2 * pi * (a / 2));
+console.log(pi * Math.pow(b, 2));
+console.log(Math.sqrt(c));
+
+
+
