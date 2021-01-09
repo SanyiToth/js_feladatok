57e720fc152aae01cdec52d84a1b78bc0ad53e80
”Tóth <sandor.toth@hotmail.com>
2021-01-09 14:38:41 +0100

    feladat2 small change

--- js_feladat2.js
+++ js_feladat2.js
@@ -5,7 +5,7 @@ b = Number.parseInt(b);
 let sum = a + b;
 let mult = a * b;
 let pi = 3.14;
-let c = Math.pow(a, a) + Math.pow(b, b);
+let cPow = Math.pow(a, a) + Math.pow(b, b);
 
 console.log('Összeg: ' + sum);
 console.log('Szorzat: ' + mult);
@@ -13,7 +13,7 @@ console.log('A hatványa önmagával: ' + Math.pow(a, a));
 console.log('A hatványa b-vel: ' + Math.pow(a, b));
 console.log('Kör kerülete: ' + 2 * pi * (a / 2));
 console.log('Kör területe: ' + pi * Math.pow(b, 2));
-console.log('Pitagorasz c: ' + Math.sqrt(c));
+console.log('Pitagorasz c: ' + Math.sqrt(cPow));
 
 
 
