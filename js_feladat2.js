688413a1d53144dd5a053fa1cf0366f52679e47c
”Tóth <sandor.toth@hotmail.com>
2021-01-09 14:35:01 +0100

    js_feladat2 changes

--- js_feladat2.js
+++ js_feladat2.js
@@ -1,17 +1,19 @@
 let a = window.prompt("Add meg 'a' értékét!");
 let b = window.prompt("Add meg 'b' értékét!");
+a = Number.parseInt(a);
+b = Number.parseInt(b);
 let sum = a + b;
 let mult = a * b;
 let pi = 3.14;
 let c = Math.pow(a, a) + Math.pow(b, b);
 
-console.log(sum);
-console.log(mult);
-console.log(Math.pow(a, a));
-console.log(Math.pow(a, b));
-console.log(2 * pi * (a / 2));
-console.log(pi * Math.pow(b, 2));
-console.log(Math.sqrt(c));
+console.log('Összeg: ' + sum);
+console.log('Szorzat: ' + mult);
+console.log('A hatványa önmagával: ' + Math.pow(a, a));
+console.log('A hatványa b-vel: ' + Math.pow(a, b));
+console.log('Kör kerülete: ' + 2 * pi * (a / 2));
+console.log('Kör területe: ' + pi * Math.pow(b, 2));
+console.log('Pitagorasz c: ' + Math.sqrt(c));
 
 
 
